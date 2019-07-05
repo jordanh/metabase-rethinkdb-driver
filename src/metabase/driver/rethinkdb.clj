20b83d3c36123dc7f2cb5e81787d4f360c32e3f0
Jordan Husney <jordan.husney@gmail.com>
2019-07-04 19:44:29 -0700

    driver/describe-database

--- src/metabase/driver/rethinkdb.clj
+++ src/metabase/driver/rethinkdb.clj
@@ -6,14 +6,28 @@
             [metabase.query-processor.store :as qp.store]
             [rethinkdb.query :as r]
             [metabase.driver.rethinkdb
-              [util :refer [details-to-clj-rethinkdb-params]]]))
+              [util :refer [*-to-clj-rethinkdb-params]]]))
 
 (driver/register! :rethinkdb)
 
 (defmethod driver/supports? [:rethinkdb :basic-aggregations] [_ _] false)
 
-(defmethod driver/can-connect? :rethinkdb [_ details]
+(defmethod driver/can-connect? :rethinkdb
+  [_ details]
   (log/info (format "driver/can-connect? details: %s" details))
-  (let [clj-rethinkdb-params (details-to-clj-rethinkdb-params details)]
+  (let [clj-rethinkdb-params (*-to-clj-rethinkdb-params details)]
+    (log/info (format "driver/describe-database: clj-rethinkdb-params=%s" clj-rethinkdb-params))
     (with-open [conn (apply r/connect (mapcat identity clj-rethinkdb-params))]
       (-> (r/now) (r/gt 0) (r/run conn)))))
+
+(defmethod driver/describe-database :rethinkdb
+  [driver database]
+  (log/info (format "driver/describe-database: driver=%s database=%s" driver database))
+  (let [clj-rethinkdb-params (*-to-clj-rethinkdb-params database)]
+    (log/info (format "driver/describe-database: clj-rethinkdb-params=%s" clj-rethinkdb-params))
+    (with-open [conn (apply r/connect (mapcat identity clj-rethinkdb-params))]
+      {:tables
+        (set
+          (for [table-name (-> (r/table-list) (r/run conn))]
+            {:name table-name
+            :schema nil}))})))
--- src/metabase/driver/rethinkdb/util.clj
+++ src/metabase/driver/rethinkdb/util.clj
@@ -3,9 +3,25 @@
             [metabase
               [util :as u]]))
 
-(defn details-to-clj-rethinkdb-params
+(defn- database->details
+  "Make sure DATABASE is in a standard db details format. This is done so we can accept several different types of
+   values for DATABASE, such as plain strings or the usual MB details map."
+  [database]
+  (cond
+    (string? database)            {:dbname database}
+    (:dbname (:details database)) (:details database) ; entire Database obj
+    (:dbname database)            database            ; connection details map only
+    :else                         (throw (Exception. (str "bad connection details:"
+                                                          (:details database))))))
+
+(defn- details-to-clj-rethinkdb-params
   [{:keys [host dbname port]}]
     {:host        host
      :db          dbname
      :port        port
     })
+
+(defn *-to-clj-rethinkdb-params
+  [maybe-details-or-database]
+  (let [details (database->details maybe-details-or-database)]
+    (details-to-clj-rethinkdb-params details)))
