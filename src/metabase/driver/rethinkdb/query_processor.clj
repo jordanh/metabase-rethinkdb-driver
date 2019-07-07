973fd0f93a6999bdb068d5935d1f5f3539cc7c7e
Jordan Husney <jordan.husney@gmail.com>
2019-07-07 13:56:38 -0700

    table query kinda

--- src/metabase/driver/rethinkdb/query_processor.clj
+++ src/metabase/driver/rethinkdb/query_processor.clj
@@ -1,15 +1,44 @@
 (ns metabase.driver.rethinkdb.query-processor
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [metabase.mbql
+              [util :as mbql.u]]
+            [metabase.query-processor
+              [interface :as i]
+              [store :as qp.store]]
+            [metabase.query-processor.middleware.annotate :as annotate]
+            [rethinkdb.query :as r]))
 
 ;; See: https://github.com/metabase/metabase/wiki/(Incomplete)-MBQL-Reference
 
+(defn- handle-table [{source-table-id :source-table} query-ctx]
+  (log/info (format "driver.query-processor/handle-table: source-table-id=%s" source-table-id))
+  (if-not source-table-id
+    query-ctx
+    (update query-ctx :query conj (r/table (qp.store/table source-table-id)))))
+
+(defn- generate-rethinkdb-query
+  [inner-query]
+  (log/info (format "driver.query-processor/generate-rethinkdb-query: inner-query=%s" inner-query))
+  (let [inner-query (update inner-query :aggregation 
+                      (partial mbql.u/pre-alias-and-uniquify-aggregations
+                               annotate/aggregation-name))]
+    (reduce (fn [query-ctx f]
+              (f inner-query query-ctx))
+            {:query {}}
+            [handle-table]
+    )))
+
 (defn mbql->native
   "Process and run an MBQL query."
-  [{{source-table-id :source-table} :query, :as query}]
-  (log/info (format "driver.query-processor/mbql->native: query=%s" query)))
+  [query]
+    ; (log/info (format "driver.query-processor/mbql->native: query=%s" query))
+  (let [native-query (generate-rethinkdb-query (:query query))]
+    (log/info (format "driver.query-processor/mbql->native: native-query=%s" native-query))
+    {:query native-query
+     :mbql? true}))
 
 (defn execute-query
   "Process and run a native RethinkDB query."
-  [{{:keys [collection query mbql? projections]} :native}]
+  [{{:keys [mbql? query]} :native}]
   (log/info (format "driver.query-processor/execute-query: query=%s" query)))
   
